286d230088c1cf3184d189c8d4e01a7d44659b38
pavel kan <pashakan030504@gmail.com>
2025-02-12 01:55:24 +0500

    lab4

---
+++ Lab4/home-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [RouterModule],
+})
+export class AppComponent {
+  title = 'home-app';
+}
